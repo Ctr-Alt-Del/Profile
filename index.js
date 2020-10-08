3df64e60fdffb8ba1946d688c8ea0856c5b50265
Leigh-Jean <ljxrustin@gmail.com>
2020-10-08 14:35:33 +0200

    Added Files

---
+++ index.js
@@ -0,0 +1,12 @@
+const checkpoint = 50;
+ 
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 1 - currentScroll / checkpoint;
+  } else {
+    opacity = 0;
+  }
+  document.querySelector(".tip").style.opacity = opacity;
+  document.querySelector(".arrow").style.opacity = opacity;
+});
